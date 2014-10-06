ee9486926f04699c82d8395dd50a37bd718c08d6
charlie <charlie.eissen@gmail.com>
2014-10-06 19:30:10 +0200

    fix for rails 3

--- lib/i18n/backend/active_record/translation.rb
+++ lib/i18n/backend/active_record/translation.rb
@@ -73,7 +73,7 @@ module I18n
           end
 
           def available_locales
-            Translation.distinct(:locale).map { |t| t.locale.to_sym }
+            Translation.uniq.pluck(:locale).map { |t| t.locale }
           end
         end
 
